fdde2408f208c681f84c15378be025e56408f11a
emporteme <magzhankarataev02@gmail.com>
2023-04-08 23:41:51 +0600

    Map is working | Connected to API

--- pages/map/index.jsx
+++ pages/map/index.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import MainLayout from "@/components/MainLayout";
+import ApiClient from "@/api/ApiClient"; // Import ApiClient
 import "leaflet/dist/leaflet.css";
-import shopData from "../../data/shop.json";
 
 const LeafletMap = dynamic(
     () => import("@/components/LeafletMap"),
     { ssr: false } // disable server-side rendering
 );
 
-function Map() {
+function Map({ shopLocations }) {
     return (
         <MainLayout>
-            <LeafletMap shopLocations={shopData.shops} />
+            <LeafletMap shopLocations={shopLocations} />
         </MainLayout>
     );
 }
 
 export default Map;
+
+export async function getServerSideProps() {
+    try {
+        const response = await ApiClient.get("/shop"); // Replace with your API endpoint
+        const shopLocations = response.data.map(shop => ({
+            id: shop.id,
+            lat: shop.latitude,
+            lng: shop.longitude,
+        }));
+
+        return {
+            props: {
+                shopLocations,
+            },
+        };
+    } catch (error) {
+        console.error("Error fetching shop data:", error);
+        return {
+            notFound: true,
+        };
+    }
+}
